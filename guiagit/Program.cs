ebe242c9207add263d5454bb68cc6bf7b543ac2b
kevnefeg <62613609+kevnefeg@users.noreply.github.com>
2021-03-11 14:25:04 -0600

    Creation

---
+++ guiagit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace guiagit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            Console.WriteLine("Enter a value for a: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {Addnumbers(a,b)}");
+        }
+        static int Addnumbers(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
